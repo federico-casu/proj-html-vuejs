d0835e7a2f64c55818bcb5de0100d188cf05aeb3
federico-casu <federico.casu1101@gmail.com>
2024-02-19 18:37:00 +0100

    feat: added Section1 component

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -1,8 +1,14 @@
 
+
 <script>
     // Js componente
+    import Section1 from './Section1.vue';
+
     export default {
         name: 'AppMain',
+        components: {
+            Section1
+        },
         data() {
             return {
                 
@@ -17,18 +23,7 @@
 
 <template>
     <!-- html componente -->
-    <section id="experience" class="">
-        <div class="container">
-            <div class="col">
-
-            </div>
-            <div class="col">
-                <span>12 year experience</span>
-                <h2>we are the best gaming company</h2>
-                <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, at tellus. Cras ultricies ligula sed magna ivamus magna justo, dictum porta.</p>
-            </div>
-        </div>
-    </section>
+    <Section1 />
     
 </template>
 
@@ -36,26 +31,5 @@
 <style lang="scss" scoped>
     /* stile del componente */
 
-    @use '../../assets/styles/partials/variables' as *;
-    @use '../../assets/styles/partials/mixins' as *;
-
-    #experience {
-        background-color: $dark-blue;
-        height: 500px;
-
-        .container {
-            display: flex;
-            justify-content: center;
-            height: 100%;
-
-            .col {
-                flex-basis: 50%;
-                border: 1px solid lime;
-                height: 100%;
-                padding: 1rem;
-                color: white;
-            }
-        }
-    }
     
 </style>
---
+++ src/components/main/Section1.vue
@@ -0,0 +1,224 @@
+
+<script>
+    // Js componente
+    export default {
+        name: 'Section1',
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+
+<template>
+    <!-- html componente -->
+    <section id="experience" class="">
+        <div class="container">
+            <div class="col images">
+                <figure>
+                    <img id="main-img" src="../../assets/img/exprince.jpg" alt="">
+                    <img id="shape-1" src="../../assets/img/shape1.png" alt="">
+                    <img id="shape-2" src="../../assets/img/shape2.png" alt="">
+                    <img id="exp-shape" src="../../assets/img/exprience-shape.png" alt="">
+                </figure>
+            </div>
+            <div class="col text">
+                <span>12 year experience</span>
+                <h2>we are the best gaming company</h2>
+                <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna ivamus magna justo, dictum porta.</p>
+                <p>Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula.</p>
+                <ul>
+                    <li>
+                        <svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>
+                        <span><strong>Sed porttitor lectus nibh. Proin eget tortor risus.</strong></span>
+                    </li>
+                    <li>
+                        <svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>
+                        <span><strong>Praesent sapien massa, convallis a pellentesque nec. risus.</strong></span>
+                    </li>
+                    <li>
+                        <svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>
+                        <span><strong>Praesent sapien massa, convallis a pellentesque nec. risus.</strong></span>
+                    </li>
+                    <li>
+                        <svg class="svg-inline--fa fa-check" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"></path></svg>
+                        <span><strong>Praesent sapien massa, convallis a pellentesque nec. risus.</strong></span>
+                    </li>
+                </ul>
+                <div class="buttons">
+                    <button>
+                        <span>discover more</span>
+                        <svg class="svg-inline--fa fa-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M504.3 273.6l-112.1 104c-6.992 6.484-17.18 8.218-25.94 4.406c-8.758-3.812-14.42-12.45-14.42-21.1L351.9 288H32C14.33 288 .0002 273.7 .0002 255.1S14.33 224 32 224h319.9l0-72c0-9.547 5.66-18.19 14.42-22c8.754-3.809 18.95-2.075 25.94 4.41l112.1 104C514.6 247.9 514.6 264.1 504.3 273.6z"></path></svg>
+                    </button>
+                    <button>
+                        <svg class="svg-inline--fa fa-play" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg=""><path fill="currentColor" d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"></path></svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+</template>
+
+
+<style lang="scss" scoped>
+    /* stile del componente */
+
+    @use '../../assets/styles/partials/variables' as *;
+    @use '../../assets/styles/partials/mixins' as *;
+
+    #experience {
+        background-color: $dark-blue;
+        
+
+        .container {
+            display: flex;
+            justify-content: center;
+
+            .col {
+                flex-basis: 50%;
+                border: 1px solid lime;
+                margin-block: 200px 100px;
+                
+                padding: 3rem;
+                color: white;
+
+            }
+            .text {
+                display: flex;
+                flex-direction: column;
+                gap: 1rem;
+                font-size: 14px;
+
+                > span {
+                    text-transform: uppercase;
+                    color: $full-green;
+                }
+
+                h2 {
+                    text-transform: capitalize;
+                    font-size: 2.2rem;
+                }
+
+                ul {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 0.5rem;
+                    li {
+                        list-style-type: none;
+                        @include d-flex-align-center;
+                        gap: 0.5rem;
+                        svg {
+                            height: 20px;
+                            aspect-ratio: 1;
+                            border: 1px solid $full-green;
+                            color: $full-green;
+                            border-radius: 50%;
+                            padding: 2px;
+                        }
+                    }
+                }
+
+                .buttons {
+                @include d-flex-align-center;
+                gap: 1.5rem;
+
+                > button {
+                    color: $deep-blue;
+                    background-color: $light-green;
+                    border: 0;
+                    height: 50px;
+                    cursor: pointer;
+                }
+
+                button:first-child {
+                    @include d-flex-align-center;
+                    border-radius: 5px;
+                    padding: 0.5rem 1rem;
+                    gap: 0.5rem;
+                    
+                    > span {
+                        text-transform: capitalize;
+                        // width: fit-content;
+                    }
+                    &:hover > svg {
+                        position: relative;
+                        left: 0.3rem;
+                    }
+
+                    > svg {
+                        height: 15px;
+                        
+                    }
+                }
+
+                button:last-child { 
+                    aspect-ratio: 1;
+                    @include d-flex-align-center;
+                    justify-content: center;
+                    border-radius: 50%;
+                    padding: 1rem;
+
+                    > svg {
+                        height: 100%;
+                        text-align: center;
+                    }
+                }
+            }
+            }
+
+            .images {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                > figure {
+                    position: relative;
+                    width: 80%;
+                    aspect-ratio: 1;
+                    
+                    #main-img {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        z-index: 1;
+                    }
+
+                    #shape-1,
+                    #shape-2 {
+                        position: absolute;
+                        
+                    }
+
+                    #shape-1 {
+                        top: -10%;
+                        left: -10%;
+                        
+                    }
+
+                    #shape-2 {
+                        width: 50%;
+                        bottom: -5%;
+                        right: -5%;
+                    }
+
+                    #exp-shape {
+                        position: absolute;
+                        width: 20%;
+                        z-index: 1;
+                        top: -5%;
+                        left: -22%;
+                    }
+                }
+
+                
+
+            }
+        }
+    }
+    
+</style>
